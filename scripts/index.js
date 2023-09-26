1265280036d0996ca844dfd5388a324eed9aa95e
Zyad Abdelsalam <zyadheshamahmed@gmail.com>
2023-09-26 23:32:03 +0300

    modified navbar, added remaining sections

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,14 @@
+$(function () {
+
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".navbar-area").removeClass("sticky");
+        } else {
+            $(".navbar-area").addClass("sticky")
+        }
+    });
+
+});
 var wow = new WOW();
 wow.init();
