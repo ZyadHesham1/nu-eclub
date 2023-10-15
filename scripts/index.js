3820d1db7ed422da0484b7760e8c453f4eae67a6
Zyad Abdelsalam <zyadheshamahmed@gmail.com>
2023-10-15 04:06:46 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,41 @@ function revealContent() {
       button.textContent = "Wanna Join?";
     }
 }
+
+const form = document.querySelector("form");
+const nextBtn = form.querySelector(".next-btn");
+const backBtn = form.querySelector(".back-btn");
+const allInput = form.querySelector(".registration input");
+
+// Hide the second form.
+document.querySelector(".form-group2").style.display = "none";
+
+nextBtn.addEventListener("click", () => {
+  // Loop through all input fields and check if they are empty.
+  allInput.forEach(input => {
+    if (input.value === "") {
+      // If any input field is empty, prevent the form from submitting.
+      form.classList.remove("secActive");
+      alert("Please fill in all required fields.");
+      return;
+    }
+  });
+
+  // If all input fields are filled in, display the next section of the form.
+  form.classList.add("secActive");
+
+  // Display the second form.
+  document.querySelector(".form-group2").style.display = "block";
+});
+
+backBtn.addEventListener("click", () => {
+  // Display the previous section of the form.
+  form.classList.remove("secActive");
+
+  // Hide the second form.
+  document.querySelector(".form-group2").style.display = "none";
+});
+
+nextBtn.addEventListener("click", () => {
+  console.log("Next button clicked!");
+});
